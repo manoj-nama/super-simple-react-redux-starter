f422611c84690c39df595a0940ba1dce0cd1a5fe
Manoj Nama <manoj.nama@outlook.com>
2016-06-26 12:20:34 +0530

    Adding basic pagination component

---
+++ app/components/pager.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Pager extends Component {
+	render() {
+		return (
+			<div>
+				<h3>Call me on my Pager</h3>
+			</div>
+		);
+	}
+}
