4764a0eef5d14195ad865af7fdddd4587356a705
Manoj Nama <manoj.nama@outlook.com>
2016-06-26 13:57:33 +0530

    Adding paging reducer to manage pagination

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import filterReducer from './filter.reducer';
 import loanReducer from './loan.reducer';
+import pagingReducer from './paging.reducer';
 
 const rootReducer = combineReducers({
 	loans: loanReducer,
 	filters: filterReducer,
+	paging: pagingReducer,
 });
 
 export default rootReducer;
---
+++ app/reducers/paging.reducer.js
@@ -0,0 +1,13 @@
+import * as types from '../actions';
+
+const initialState = {
+	total: 0,
+	hits: 0,
+	pages: 0
+};
+
+const pagingReducer = function(state = initialState, action) {
+	return state;
+}
+
+export default pagingReducer;
