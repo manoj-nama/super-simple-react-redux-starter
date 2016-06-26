e7f569b7945e9fa1043544b684860e62f09e318c
Manoj Nama <manoj.nama@outlook.com>
2016-06-26 12:26:46 +0530

    Adding the simple row component

---
+++ app/components/row.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Row extends Component {
+	render() {
+		return (
+			<div>
+				<h2> This is a simple row </h2>
+			</div>
+		);
+	}
+}
