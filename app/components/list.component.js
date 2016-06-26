1f0c3b2dc75bb3779b6bc71acb415a076a6d4bdf
Manoj Nama <manoj.nama@outlook.com>
2016-06-26 12:20:09 +0530

    Adding basic list component

---
+++ app/components/list.component.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class List extends Component {
+	render() {
+		return (
+			<div>
+				<h3>Items!!!</h3>
+				<ul>
+					{
+						this.props.items.map((i) => {
+							return (<li key={i}><em>{i}</em></li>)
+						})
+					}
+				</ul>
+				<p>
+					{this.props.loading ? 'Loading items ...' : 'Completed loading ...'}
+				</p>
+			</div>
+		);
+	}
+}
