3c73c08d54c6ba289ce911b565181af0a293033b
Manoj Nama <manoj.nama@outlook.com>
2016-06-26 11:57:32 +0530

    Adding constants for actions

---
+++ app/actions/index.js
@@ -0,0 +1,40 @@
+export const REQUEST_LOANS = 'REQUEST_LOANS';
+export const RECIEVE_LOANS = 'RECIEVE_LOANS';
+export const UPDATE_FILTERS = 'UPDATE_FILTERS';
+export const CHANGE_PAGE = 'CHANGE_PAGE';
+export const SELECT_LOAN = 'SELECT_LOAN';
+
+export function requestLoans(filters) {
+	return {
+		type: REQUEST_LOANS,
+		filters
+	}
+}
+
+export function recieveLoans(loans) {
+	return {
+		type: RECIEVE_LOANS,
+		loans
+	}
+}
+
+export function updateFilters(filters) {
+	return {
+		type: UPDATE_FILTERS,
+		filters
+	}
+}
+
+export function changePage(page) {
+	return {
+		type: CHANGE_PAGE,
+		page
+	}
+}
+
+export function  selectLoan(loan) {
+	return {
+		type: SELECT_LOAN,
+		loan
+	}
+}
