58c96d3967b013818d4e01bddfe6b53e06f153ef
Manoj Nama <manoj.nama@outlook.com>
2016-06-26 19:11:27 +0530

    Adding sticky scroll to the component

--- app/components/filter.component.js
+++ app/components/filter.component.js
@@ -1,11 +1,41 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Filter extends Component {
+
+	constructor(props) {
+		super(props);
+		this.elementOffset = 0;
+	}
+
+	componentDidMount() {
+		let el = this.refs.filterComponent;
+		this.elementOffset = el.offsetTop;
+		document.addEventListener("scroll", this._stickyScroll.bind(this), false);
+	}
+
+	_stickyScroll() {
+		let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		if (scrollTop > this.elementOffset) {
+			//make it sticky
+			document.body.classList.add("sticky-el");
+		} else {
+			document.body.classList.remove("sticky-el");
+		}
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener("scroll", this._stickyScroll.bind(this), false);
+	}
+
 	render() {
 		return (
-			<div className="filter-component">
-				This is filter component
+			<div className="filter-component" ref="filterComponent">
+				<i className="fa fa-filter"></i> Filters
 				<button disabled={this.props.loading} onClick={this.props.onUpdate}> Update list </button>
+
+				<a className="filter-dropdown" href="javascript:void(0)">
+					<i className="fa fa-chevron-down"></i>
+				</a>
 			</div>
 		);
 	}
