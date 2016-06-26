e14425ccf7041cf3c0f4f95d5b299a1e9d4efc5e
Manoj Nama <manoj.nama@outlook.com>
2016-06-26 12:00:35 +0530

    Adding basic reducer for filters

---
+++ app/reducers/filter.reducer.js
@@ -0,0 +1,9 @@
+import * as types from '../actions';
+
+const initialState = {};
+
+const filterReducer = function(state = initialState, action) {
+	return state;
+}
+
+export default filterReducer;
