8d0e0dac8e5d2e753dde3c6615b36674c15bb2bf
Manoj Nama <manoj.nama@outlook.com>
2016-06-26 12:26:04 +0530

    Adding the detail component

---
+++ app/components/detail.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Detail extends Component {
+	render() {
+		return (
+			<div>
+				<h2> This is the detail section </h2>
+			</div>
+		);
+	}
+}
